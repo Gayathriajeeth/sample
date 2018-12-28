f971834a979553c473905bcaf9b8c86d3d7ca0f7
Gayathriajeeth <gayathri.an@seqato.com>
2018-12-28 12:55:44 +0530

    commited

--- src/main/java/com/seqato/sample/App.java
+++ src/main/java/com/seqato/sample/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("hi");
-        System.out.println("hello");
+        System.out.println("hello sreejith");
     }
 }
